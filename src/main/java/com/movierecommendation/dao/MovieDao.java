e00d92db0aacd259cc8c4d9a8830c9604cca65d3
YourGitHubUsername <jmwaura@byupathway.edu>
2025-07-20 01:13:12 +0300

    COMMIT

--- src/main/java/com/movierecommendation/dao/MovieDao.java
+++ src/main/java/com/movierecommendation/dao/MovieDao.java
@@ -1,30 +1,12 @@
-// dao/MovieDAO.java
-package com.movieapp.dao;
+package com.movierecommendation.dao;
 
-import com.movieapp.model.Movie;
+import com.movierecommendation.model.Movie;
 import java.util.List;
-import java.util.Optional;
 
 public interface MovieDAO {
-    // Create
-    void save(Movie movie);
-    
-    // Read
-    Optional<Movie> findById(Long id);
-    List<Movie> findAll();
-    List<Movie> findByTitle(String title);
-    List<Movie> findByGenre(String genre);
-    List<Movie> findByYear(int year);
-    List<Movie> findByRatingRange(double minRating, double maxRating);
-    
-    // Update
-    void update(Movie movie);
-    
-    // Delete
-    void deleteById(Long id);
-    boolean existsById(Long id);
-    
-    // Statistics
-    long count();
-    double getAverageRating(Long movieId);
+    void addMovie(Movie movie);
+    Movie getMovieById(int id);
+    List<Movie> getAllMovies();
+    void updateMovie(Movie movie);
+    void deleteMovie(int id);
 }
