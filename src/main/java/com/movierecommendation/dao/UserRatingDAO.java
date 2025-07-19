488e66584c82a526cdabdeb8fd52a611c84131f4
YourGitHubUsername <jmwaura@byupathway.edu>
2025-07-20 01:11:45 +0300

    COMMIT

--- src/main/java/com/movierecommendation/dao/UserRatingDAO.java
+++ src/main/java/com/movierecommendation/dao/UserRatingDAO.java
@@ -1,35 +1,13 @@
-// dao/UserRatingDAO.java
-package com.movieapp.dao;
+package com.movierecommendation.dao;
 
-import com.movieapp.model.UserRating;
+import com.movierecommendation.model.UserRating;
 import java.util.List;
-import java.util.Optional;
 
 public interface UserRatingDAO {
-    // Create
-    void save(UserRating rating);
-    
-    // Read
-    Optional<UserRating> findById(Long id);
-    List<UserRating> findAll();
-    List<UserRating> findByUserId(Long userId);
-    List<UserRating> findByMovieId(Long movieId);
-    Optional<UserRating> findByUserIdAndMovieId(Long userId, Long movieId);
-    List<UserRating> findByRatingValue(int rating);
-    List<UserRating> findByRatingRange(int minRating, int maxRating);
-    
-    // Update
-    void update(UserRating rating);
-    
-    // Delete
-    void deleteById(Long id);
-    void deleteByUserIdAndMovieId(Long userId, Long movieId);
-    boolean existsByUserIdAndMovieId(Long userId, Long movieId);
-    
-    // Statistics
-    long count();
-    long countByMovieId(Long movieId);
-    long countByUserId(Long userId);
-    double getAverageRatingForMovie(Long movieId);
-    List<UserRating> getTopRatedMoviesByUser(Long userId, int limit);
+    void addUserRating(UserRating userRating);
+    UserRating getUserRating(int userId, int movieId);
+    List<UserRating> getUserRatingsByUser(int userId);
+    List<UserRating> getUserRatingsByMovie(int movieId);
+    void updateUserRating(UserRating userRating);
+    void deleteUserRating(int userId, int movieId);
 }
